07d2fbb8f106394b71f6f1f49d7a4c19fbb932f1
Matt Kubej <mkubej@gmail.com>
2021-04-03 00:56:34 -0400

    move treesitter configs

--- nvim/.config/nvim/lua/mk/plugins/init.lua
+++ nvim/.config/nvim/lua/mk/plugins/init.lua
@@ -1,11 +1,11 @@
 require('mk.plugins.startup')
 
 require('mk.plugins.telescope')
-require('mk.plugins.treesitter')
 
 require('mk.plugins.configs.ale')
+require('mk.plugins.configs.base16')
 require('mk.plugins.configs.easyalign')
 require('mk.plugins.configs.lualine')
 require('mk.plugins.configs.nerdtree')
+require('mk.plugins.configs.treesitter')
 require('mk.plugins.configs.vimwiki')
-require('mk.plugins.configs.base16')

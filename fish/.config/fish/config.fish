0b07639c9b73232d17181b0272f116f7a2706ace
Matt Kubej <mkubej@gmail.com>
2021-05-03 22:46:19 -0400

    attach to default session

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,8 +12,9 @@ function fish_user_key_bindings
 end
 
 # startup fish in tmux
-if status --is-interactive
-  tmux ^ /dev/null; and exec true
+if status is-interactive
+  and not set -q TMUX
+  tmux attach -t default || tmux new -s default ^ /dev/null; and exec true
 end
 
 # override ls with exa, if exists

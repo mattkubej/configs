c0cc9ea57227ce3fd03b30f08c87e1d369566d58
Matt Kubej <mkubej@gmail.com>
2021-05-24 21:23:58 -0400

    change forward-char binding

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,9 @@ fish_vi_key_bindings
 # scratchpad for quick note taking
 alias sp='nvim $HOME/notes/scratchpad-(date +"%m-%d-%Y").md'
 
-# allow for ctrl+f in vi-mode
-# https://github.com/fish-shell/fish-shell/issues/3541#issuecomment-260001906
 function fish_user_key_bindings
     for mode in insert default visual
-        bind -M $mode \cf forward-char
+        bind -M $mode \cn forward-char
     end
 end
 

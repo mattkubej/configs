d5bb50feebb883cb027a60b7bab58805e3c8f22b
Matt Kubej <mkubej@gmail.com>
2021-08-14 12:25:19 -0400

    add formatter

--- nvim/.config/nvim/lua/mk/plugins/configs/formatter.lua
+++ nvim/.config/nvim/lua/mk/plugins/configs/formatter.lua
@@ -11,6 +11,16 @@ require('formatter').setup({
           }
         end
     },
+    typescriptreact = {
+        -- prettier
+       function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+            stdin = true
+          }
+        end
+    },
   }
 })
 
